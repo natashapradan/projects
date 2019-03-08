1e26009db622f60bc10bee3611ae4cb9280f6fef
natashapradan <natasha.pradan@gmail.com>
2019-03-08 19:12:00 +0200

    Updated everything

---
+++ 16-project/src/components/DropdownOptions.vue
@@ -0,0 +1,18 @@
+<template>
+  <b-dropdown id="ddown1" :text="$t('changeLanguage')" class="m-md-2">
+    <b-dropdown-item class="ml-auto" v-for="option in options" @click="$emit('input', option[optionValue])">
+        {{option[optionKey]}}
+    </b-dropdown-item>
+  </b-dropdown>
+</template>
+
+<script>
+    export default {
+        name: "DropdownOptions",
+        props: {
+            options: {required: true, type: Array},
+            optionKey: {required: true, type: String},
+            optionValue: {required: true, type: String}
+        }
+    }
+</script>
