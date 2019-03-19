16609cbb78dfbea7c4245994c7d60894448ad69a
natashapradan <natasha.pradan@gmail.com>
2019-03-19 13:25:18 +0200

    FundRaiser

---
+++ 23-project/src/EuropeAccelerators.vue
@@ -0,0 +1,65 @@
+<template>
+  <div v-if="europeanCountry" class="d-flex flex-row flex-wrap">
+    <b-card
+        v-for="opportunity in europeanCountry.opportunities"
+        :title="opportunity.name"
+        :img-src="opportunity.image"
+        img-alt="Image"
+        img-top
+        tag="article"
+        style="max-width: 20rem; border: 1px solid #2C3E50; text-transform: capitalize"
+        class="m-5"
+    >
+      <p><em>{{ opportunity.description }}</em></p>
+      <b-row>
+        <b-col>
+          Funds: <h4>{{ opportunity.funds }}</h4> to each participant
+        </b-col>
+        <b-col>
+          Terms: <h4>{{ opportunity.terms }}</h4> ownership upon success
+        </b-col>
+      </b-row>
+      <hr/>
+      <b-row class="mt-2">
+        <b-col>
+          Relocation to: <strong>{{ opportunity.relocation }}</strong>
+        </b-col>
+        <b-col>
+          Duration: <strong>{{ opportunity.duration }}</strong>
+        </b-col>
+      </b-row>
+      <hr/>
+      MVP: <strong>{{ opportunity.mvp }}</strong>
+      <hr/>
+      Deadline: <h4 class="style: text-danger">{{ opportunity.deadline }}</h4>
+
+      <b-button :href="opportunity.apply" class="btn btn-success btn-block mt-2">
+        <strong>Apply</strong>
+      </b-button>
+    </b-card>
+  </div>
+
+</template>
+
+<script>
+  import axios from "axios"
+
+
+  export default {
+    name: 'products',
+    data() {
+      return {
+        title: 'products',
+        europeanCountry: {opportunities: []},
+      }
+    },
+    async created() {
+
+      const response = await axios.get('http://localhost:3000/europeanCountries/' + this.$route.params.id)
+      this.europeanCountry = response.data;
+    }
+  }
+  //
+</script>
+
+
---
+++ 23-project/src/Home.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <b-container class="container-fluid">
+      <h2 class="mt-4">Opportunities are there</h2>
+      <b-row class="mt-3">
+        <b-col sm="12" lg="6" class="mb-4">
+          <b-card header="Accelerators" class="text-left">
+            They are divided into two categories: some ask for equity % in exchange for training,
+            networking and, most importantly, help in raising capital introducing you to their trusted investors and
+            hosting special Demo Days; others act for
+            free as they have an agreement with investors or governments.
+          </b-card>
+        </b-col>
+        <b-col sm="12" lg="6" class="mb-4">
+          <b-card header="Business Angels/Venture Funds" class="text-left">
+            These are individuals or entities aiming at investing early with high risk in order to get a
+            high percent of future equity. Sometimes companies rely only on business angels' capital and never take
+            part in accelerators; however, accelerators provide more than just capital through their mentors.
+          </b-card>
+        </b-col>
+      </b-row>
+      <h3 class="mt-4">Both these opportunities do not exclude each other</h3>
+      <h4 class="mt-4">The majority of startups turn to investment sooner or later</h4>
+    </b-container>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    name: 'home',
+    data() {
+      return {
+      }
+    }
+  }
+</script>
+
